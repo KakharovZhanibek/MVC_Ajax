11669d1e8b7f945c9ec8c1fa95106129241e7a4e
КахаровЖ <@N215-2>
2020-02-17 21:18:04 +0600

    Add project files.

---
+++ MVC_Ajax/Controllers/MainController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Ajax.Models;
+
+namespace MVC_Ajax.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+
+        private readonly List<Book> db;
+        public MainController()
+        {
+            db = new List<Book>()
+            {
+                new Book{Name="book1",Author="Nick Harson"},
+                new Book{Name="book2",Author="Shild"},
+                new Book{Name="C++",Author="Shild"},
+                new Book{Name="book3",Author="Figa"},
+                new Book{Name="book4",Author="Troelsen"},
+                new Book{Name="book5",Author="Jimmy"}
+            };
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BookSearch(string name)
+        {
+            var allbooks = db.Where(a => a.Author.Contains(name)).ToList();
+            if (allbooks.Count <= 0)
+            {
+                return new HttpUnauthorizedResult();
+            }
+            return PartialView(allbooks);
+        }
+    }
+}
